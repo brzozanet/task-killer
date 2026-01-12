a48bfdfbf2a5b73a530a74bad62e999e99e474ca
Paweł Brzozowski <pawel@brzoza.net>
2026-01-12 19:53:01 +0100

    feat: enhance button tests to verify rendering and click behavior

--- tests/integration/button.test.jsx
+++ tests/integration/button.test.jsx
@@ -1,7 +1,15 @@
-import { render } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import { render, screen, fireEvent } from "@testing-library/react";
 import { Button } from "../../src/components/Button/Button";
 
-test("Button integration test", () => {
-  const { container } = render(<Button>Click me!</Button>);
-  expect(container).toBeInTheDocument();
+test("Properly renders passed text", () => {
+  render(<Button>Kliknij mnie!</Button>);
+  expect(screen.getByRole("button")).toHaveTextContent("Kliknij mnie!");
+});
+
+test("Calls onClick callback after being clicked", () => {
+  const onClickCallback = jest.fn(() => console.log("Button clicked"));
+  render(<Button onClick={onClickCallback} />);
+  fireEvent.click(screen.getByRole("button"));
+  expect(onClickCallback).toHaveBeenCalled();
 });
