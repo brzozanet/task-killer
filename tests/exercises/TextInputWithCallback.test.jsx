3f91095a0b4825fa3a76abe24e8b32a21e618a0a
Paweł Brzozowski <pawel@brzoza.net>
2026-01-12 19:54:20 +0100

    feat: remove unused TextInputWithCallback and useDarkMode components and their tests

--- tests/exercises/TextInputWithCallback.test.jsx
+++
@@ -1,2 +0,0 @@
-import { render, screen, fireEvent } from "@testing-library/react/pure";
-import { TextInputWithCallback } from "../../src/exercises/TextInputWithCallback";
--- tests/exercises/useDarkMode.test.js
+++
@@ -1,3 +0,0 @@
-import "@testing-library/jest-dom";
-import { renderHook, act } from "@testing-library/react/pure";
-import { useDarkMode } from "../../src/exercises/useDarkMode";
